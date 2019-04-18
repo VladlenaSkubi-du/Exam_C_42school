7bc66437ce232d4055cc29cea34bc192e1e02bdc
sschmele <sschmele@oa-j3.21-school.ru>
2019-04-18 18:16:29 +0300

    Brainfuck done

--- brainfuck/brainfuck.c
+++ brainfuck/brainfuck.c
@@ -6,48 +6,18 @@
 /*   By: sschmele <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/09 19:48:01 by sschmele          #+#    #+#             */
-/*   Updated: 2019/04/17 12:47:33 by sschmele         ###   ########.fr       */
+/*   Updated: 2019/04/18 18:10:20 by sschmele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
- * Assignment name  : brainfuck
- * Expected files   : *.c, *.h
- * Allowed functions: write, malloc, free
- * --------------------------------------------------------------------------------
- *
- *  Write a Brainfuck interpreter program.
- *  The source code will be given as first parameter.
- *  The code will always be valid, with no more than 4096 operations.
- *  Brainfuck is a minimalist language. It consists of an array of bytes
- *  (in our case, let's say 2048 bytes) initialized to zero,
- *  and a pointer to its first byte.
- *
- *  Every operator consists of a single character :
- *  - '>' increment the pointer ;
- *  - '<' decrement the pointer ;
- *  - '+' increment the pointed byte ;
- *  - '-' decrement the pointed byte ;
- *  - '.' print the pointed byte on standard output ;
- *  - '[' go to the matching ']' if the pointed byte is 0 (while start) ;
- *  - ']' go to the matching '[' if the pointed byte is not 0 (while end).
- *
- *  Any other character is a comment.
- *
- *  Examples:
- *
- *  $>./brainfuck "++++++++++[>+++++++>++++++++++>+++>+<<<<-]
- *  >++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>." | cat -e
- *  Hello World!$
- *  $>./brainfuck "+++++[>++++[>++++H>+++++i<<-]>>>++\n<<<<-]>>--------.>+++++.>." | cat -e
- *  Hi$
- *  $>./brainfuck | cat -e
- *  $
+**The easiest test: "+++++[>++++++++++<-]>++.--."
+**Comments: malloc is not needed, in conditions for '[' and ']' if is very
+**necessary that i++ and i-- stay before ifs.
 */
 
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 void		brainfuck(char *s, char *str)
 {
@@ -58,60 +28,40 @@ void		brainfuck(char *s, char *str)
 	i = 0;
 	j = 0;
 	flag = 0;
-	printf("here\n");
 	while (s[i])
 	{
 		if (s[i] == '+')
-		{
 			str[j]++;
-			printf("plus: ptr =%d	ascii =%d	char =%c\n", j, str[j], str[j]);
-		}
 		else if (s[i] == '-')
-		{
 			str[j]--;
-			printf("minus: ptr =%d   ascii =%d   char =%c\n", j, str[j], str[j]);
-		}
 		else if (s[i] == '>')
-		{
 			j++;
-			printf(">: ptr =%d   ascii =%d   char =%c\n", j, str[j], str[j]);
-		}
 		else if (s[i] == '<')
-		{
 			j--;
-			printf("<: ptr =%d   ascii =%d   char =%c\n", j, str[j], str[j]);
-		}
 		else if (s[i] == '.')
-			printf("print: %c\n", str[j]);
-		else if (s[i] == '[')
+			write(1, str + j, 1);
+		if (s[i] == '[' && str[j] == 0)
 		{
-			if (str[j] == 0)
+			flag = 1;	
+			while (flag)
 			{
-				str[j]++;
-				while (str[j])
-				{
-					i++;
-					if (s[i] == '[')
-						str[j]++;
-					if (s[i] == ']')
-						str[j]--;
-				}
+				i++;
+				if (s[i] == '[')
+					flag++;
+				else if (s[i] == ']')
+					flag--;
 			}
 		}
-		else if (s[i] == ']')
+		if (s[i] == ']' && str[j])
 		{
-			if (str[j])
+			flag = 1;
+			while (flag)
 			{
-				str[j]++;
-				while (str[j])
-				{
-					i--;
-					if (s[i] == '[')
-						str[i]--;
-					if (s[i] == ']')
-						str[j]++;
-				}
 				i--;
+				if (s[i] == ']')
+					flag++;
+				else if (s[i] == '[')
+					flag--;
 			}
 		}
 		i++;
@@ -139,6 +89,7 @@ int			main(int argc, char **argv)
 	
 	if (argc == 2 && argv[1][0] && ft_strlen(argv[1]) <= 4096)
 		brainfuck(argv[1], str);
-	write(1, "\n", 1);
+	else
+		write(1, "\n", 1);
 	return (0);
 }
