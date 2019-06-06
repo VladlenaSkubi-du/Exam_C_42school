98add84af1125fb65408854b91ef9d527c8514d7
sschmele <sschmele@oa-j4.21-school.ru>
2019-06-06 14:34:33 +0300

    Traces for rpn

--- cycle_detector/cycle_detector.c
+++ cycle_detector/cycle_detector.c
@@ -6,7 +6,7 @@
 /*   By: sschmele <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/02 17:26:20 by sschmele          #+#    #+#             */
-/*   Updated: 2019/05/03 19:20:38 by sschmele         ###   ########.fr       */
+/*   Updated: 2019/06/06 14:19:15 by sschmele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@ int		cycle_detector(const t_list *list)
 	const t_list	*tmp2;
 
 	if (!list)
-		return (1);
+		return (0);
 	tmp1 = list;
 	tmp2 = tmp1;
 	while (tmp1 && tmp2)
 	{
 		if (tmp1->next)
 			tmp1 = tmp1->next;
+		else
+			return (0);
 		if (tmp2->next && tmp2->next->next)
 			tmp2 = tmp2->next->next;
 		else
